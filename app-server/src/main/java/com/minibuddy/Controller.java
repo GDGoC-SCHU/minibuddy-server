2430496348fd15341a381f343b9c295a870d157f
y <dkgk2233@gmail.com>
2025-04-10 23:56:08 +0900

    [feat] set up server with docker

---
+++ app-server/src/main/java/com/minibuddy/Controller.java
@@ -0,0 +1,12 @@
+package com.minibuddy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String home() {
+        return "hello world";
+    }
+}
