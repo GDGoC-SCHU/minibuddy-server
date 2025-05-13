8b8b81f10fb4d82e97f692bca78ccd455b2eb405
Seong57 <gustjr7297@naver.com>
2025-05-13 19:07:11 +0900

    [test] user service test

--- app-server/src/main/java/com/minibuddy/feature/user/dto/ProfileUpdateRequest.java
+++ app-server/src/main/java/com/minibuddy/feature/user/dto/ProfileUpdateRequest.java
@@ -1,11 +1,13 @@
 package com.minibuddy.feature.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class ProfileUpdateRequest {
